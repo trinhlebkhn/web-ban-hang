125f6618f9e0255a207e319ff76624260e554716
hdb.a12k34 <hdb.a1234@gmail.com>
2018-10-03 00:43:06 +0700

    fail

---
+++ apps/backend/controllers/ConfigController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: An Viet Computer
+ * Date: 9/27/2018
+ * Time: 9:29 PM
+ */
+
+namespace Graduate\Backend\Controllers;
+
+
+class ConfigController
+{
+
+}
---
+++ apps/models/Config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: An Viet Computer
+ * Date: 9/27/2018
+ * Time: 11:17 PM
+ */
+
+class Config
+{
+
+}
